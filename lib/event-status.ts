5c6e430c5d9e51f3d8b09f941ea51babc5c51a8b
HarshGeed <harshgeed07@gmail.com>
2025-12-24 10:31:21 +0530

    date type issue fixed

--- lib/event-status.ts
+++ lib/event-status.ts
@@ -1,12 +1,12 @@
 import { EventStatus } from "@/types/event";
 
 export const getEventStatus = (
-    startDate: string, 
-    endDate: string
+    startDate: string | Date, 
+    endDate: string | Date
 ) : EventStatus => {
     const now = new Date();
-    const start = new Date(startDate)
-    const end = new Date(endDate)
+    const start = startDate instanceof Date ? startDate : new Date(startDate);
+    const end = endDate instanceof Date ? endDate : new Date(endDate);
 
     if(now < start) return "UPCOMING";
     if(now >= start && now <= end) return "ONGOING"
